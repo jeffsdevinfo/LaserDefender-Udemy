169bf9f9ad951c32ef26defbcc6b6668130b1fe1
Jeff Simon <jeffsdevinfo@gmail.com>
2020-08-24 21:34:18 -0700

    first commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] float delayInSeconds = 2f;
+    Coroutine WaitBeforeEndScreen;
+
+    public void LoadStartMenu()
+    {
+        //Load something based on the scene index
+        SceneManager.LoadScene(0);
+        Debug.Log("Start Menu has been clicked");
+
+    }
+
+    public void LoadMainGame()
+    {
+        //Load something based on the name of the scene
+        // String references are not necessarily good because if the
+        // name changes in the ui editor then the reference will be broken
+        
+        SceneManager.LoadScene("Game");
+        Debug.Log("Start Game has been clicked");
+        GameSession session = FindObjectOfType<GameSession>();
+        session.ResetGame();
+    }
+
+   IEnumerator SleepBeforeEnding()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene("Game Over");
+        Debug.Log("End Game has been happended");
+    }
+
+    public void LoadGameOver()
+    {
+        WaitBeforeEndScreen = StartCoroutine(SleepBeforeEnding());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game has been clicked");
+    }
+}
---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    Scene scene;
+    MusicPlayer mp;
+    float secondsToPause = 4f;
+
+    //IEnumerator PauseBeforeStartingMusic()
+    //{
+    //    yield return new WaitForSeconds(secondsToPause);
+    //    AudioSource audioSource = GetComponent<AudioSource>();
+    //    audioSource.Play();
+
+    //}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+        if (scene.name.ToString().Equals("Game"))
+        {            
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.PlayDelayed(3);
+        }
+        else
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.PlayDelayed(1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
